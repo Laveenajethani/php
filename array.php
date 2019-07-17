a47c4e69232db072cb335f98a8209bdc1d7b5c23
Laveena Jethani <laveena.j.21@gmail.com>
2019-07-17 23:53:06 +0530

    Create array.php

---
+++ array.php
@@ -0,0 +1,14 @@
+<?php
+$x=array("amit","rahul","sameer");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>apache server</title>
+</head>
+<body>
+   <h1>hello <?php echo $x[0]?> this is learing of php</h1>
+   <h1>hello <?php echo $x[1]?> this is learing of php</h1>
+   <h1>hello <?php echo $x[2]?> this is learing of php</h1>
+</body>
+</html>

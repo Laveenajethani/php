b13b53e67a1c9282b3383f43bcd43a24c4ba8191
Laveena Jethani <laveena.j.21@gmail.com>
2019-07-17 22:13:20 +0530

    Create example.php

---
+++ example.php
@@ -0,0 +1,27 @@
+<?php
+$x="saurabh";
+$y=23;
+$z=45.56;
+?>
+<html>
+<head>
+<style>
+h1
+{
+color:blue;
+}
+p
+{
+color:green;
+}
+<script>
+document.write("this is java script");
+</script>
+</style>
+</head>
+<body>
+<?php $y++ $z="hello"; ?>
+   <p>this is a paragraph</p>
+   <h1>hello , <?php echo $x; ?>this is heading</h1>
+</body>
+</html>
